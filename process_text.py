c045ae58e0cc6f4a118300606f02943d948744d6
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-09-03 21:02:41 +0800

    Update process_text.py

--- process_text.py
+++ process_text.py
@@ -1,5 +1,10 @@
-bbl = open("Bible_KJV.txt", "r", encoding="utf-8")
-bbl_text = bbl.read()
-bbl.close()
+def clean(output):
+    bbl = open("Bible_KJV.txt", "r", encoding="utf-8")
+    bbl_text = bbl.read()
+    bbl.close()
 
-open("cleared.txt", "w", encoding="utf-8").write(bbl_text.replace("	"," "))
+    open(output, "w", encoding="utf-8").write(bbl_text.replace("	"," "))
+
+
+if __name__ == "__main__":
+    clean("cleared.txt")
