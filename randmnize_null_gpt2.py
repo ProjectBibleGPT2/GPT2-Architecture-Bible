61458c2455003dd668a04de4161f22bb4c34d452
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-09-03 21:02:56 +0800

    Update randmnize_null_gpt2.py

--- randmnize_null_gpt2.py
+++ randmnize_null_gpt2.py
@@ -1,26 +1,26 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 import os
+def randomize():
+    model_name = "Inoob/Null-GPT2-Large"
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-model_name = "Inoob/Null-GPT2-Large"
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    # Reset parameters
+    for name, param in model.named_parameters():
+        if param.dim() > 1:
+            torch.nn.init.xavier_uniform_(param)
+        else:
+            torch.nn.init.zeros_(param)
+    return model, tokenizer
+if __name__ == "__main__":
+    model,tokenizer =randomize("gpt2-large-architecture")
+    output_dir = "./gpt2-large-architecture"
+    # Save the reset model and tokenizer
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-# Reset parameters
-for name, param in model.named_parameters():
-    if param.dim() > 1:
-        torch.nn.init.xavier_uniform_(param)
-    else:
-        torch.nn.init.zeros_(param)
+    model.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
 
-
-
-# Save the reset model and tokenizer
-output_dir = "./gpt2-large-architecture"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-model.save_pretrained(output_dir)
-tokenizer.save_pretrained(output_dir)
-
-print(f"Reset model and tokenizer saved to {output_dir}")
+    print(f"Reset model and tokenizer saved to {output_dir}")
