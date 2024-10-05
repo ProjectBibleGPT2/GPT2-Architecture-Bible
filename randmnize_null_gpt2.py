c53410be3c0abb98f97e3558e005338ee99d6b6d
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-10-05 22:24:42 +0800

    Update randmnize_null_gpt2.py

--- randmnize_null_gpt2.py
+++ randmnize_null_gpt2.py
@@ -1,10 +1,17 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from tokenizers import (
+    models,
+    pre_tokenizers,
+    processors,
+    trainers,
+    Tokenizer,
+)
 import torch
 import os
 def randomize(input_model):
     model_name = input_model
     model = GPT2LMHeadModel.from_pretrained(model_name)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    
 
     # Reset parameters
     for name, param in model.named_parameters():
@@ -12,6 +19,13 @@ def randomize(input_model):
             torch.nn.init.xavier_uniform_(param)
         else:
             torch.nn.init.zeros_(param)
+    tokenizer = Tokenizer(models.BPE())
+    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+    trainer = trainers.BpeTrainer(vocab_size=25000, special_tokens=["<|endoftext|>"])
+    tokenizer.model = models.BPE()
+    tokenizer.train(["cleared.txt"], trainer=trainer)
+    tokenizer.post_processor = processors.ByteLevel(trim_offsets=False)
+    tokenizer = GPT2TokenizerFast(tokenizer_object=tokenizer)
     return model, tokenizer
 if __name__ == "__main__":
     model,tokenizer =randomize("Inoob/Null-GPT2-Large")
